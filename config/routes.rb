a059b3069251d1a391707a723be744cd846e8519
efoosag <efoosagsite@gmail.com>
2023-05-10 23:03:44 +0100

    add devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  root 'welcome#index'
+  # devise_for :users
+  devise_for :users do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+  root 'welcome#index'  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
